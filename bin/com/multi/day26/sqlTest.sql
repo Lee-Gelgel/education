90869cb6d18c981c99bac8b0fc57a57a84c20bad
이제훈 <hunlee001@naver.com>
2016-06-26 22:55:31 +0900

    2016/06/26

---
+++ bin/com/multi/day26/sqlTest.sql
@@ -0,0 +1,22 @@
+select employee_id, first_name, department_name 
+from employees join departments using (department_id)
+order by employee_id
+
+drop table member;
+
+create table member(
+	mno number primary key,
+	mname varchar2(20) not null,
+	phone varchar2(13),
+	email varchar2(20),
+	birthdate date
+);
+
+
+
+insert into member (mno,mname,phone,email,birthdate) 
+values (1,'gelgel','010-1234-1234','lee.gelgel@gmail.com',sysdate);
+insert into member (mno,mname,phone,email,birthdate) 
+values (2,'gelgel','010-1234-1234','lee.gelgel@gmail.com',sysdate);
+
+select * from member
